7c72457d2cc906d670700e5d0655e93b6faaa141
ClayTeam <64989516+clayteamcyber@users.noreply.github.com>
2020-05-08 05:47:42 +0700

    Add files via upload

---
+++ clay.sh
@@ -0,0 +1,468 @@
+#!/bin/bash
+
+
+#TOLONG JANGAN DI GANTI AUTHOR NYA YAH
+#CREATOR SCRIPT ALVIANCLAY01
+
+User="CLAY"
+pass="CLAY TEAM CYBER"
+hi='\33[0;32m' #hijau
+gl='\33[32;1m' #greenlight
+bi='\33[0;36m' #biru
+bl='\33[36;1m' #bluelight
+me='\33[31;1m' #merah
+pu='\33[1;37m' #putih
+ku='\33[33;1m' #kuning
+ulang="y"
+while [ $ulang = "y" ]:
+do
+clear
+echo ""$hi"I"
+clear
+echo ""$hi"IN"
+clear
+echo ""$hi"INS"
+clear
+echo ""$hi"INST"
+clear
+echo ""$hi"INSTA"
+clear
+echo ""$hi"INSTALL"
+clear
+echo ""$hi"INSTALL P"
+clear
+echo ""$hi"INSTALL PA"
+clear
+echo ""$hi"INSTALL PAS"
+clear
+echo ""$hi"INSTALL PASS"
+clear
+echo ""$hi"INSTALL PASSW"
+clear
+echo ""$hi"INSTALL PASSWO"
+clear
+echo ""$hi"INSTALL PASSWOR"
+clear
+echo ""$hi"INSTALL PASSWORD"
+clear
+echo ""$hi"INSTALL PASSWORD"
+clear
+echo ""$hi"INSTALL PASSWORD T"
+clear
+echo ""$hi"INSTALL PASSWORD TU"
+clear
+echo ""$hi"INSTALL PASSWORD TUN"
+clear
+echo ""$hi"INSTALL PASSWORD TUNG"
+clear
+echo ""$hi"INSTALL PASSWORD TUNGG"
+clear
+echo ""$hi"INSTALL PASSWORD TUNGGU"
+clear
+echo ""$hi"INSTALL PASSWORD TUNGGU S"
+clear
+echo ""$hi"INSTALL PASSWORD TUNGGU SE"
+clear
+echo ""$hi"INSTALL PASSWORD TUNGGU SEB"
+clear
+echo ""$hi"INSTALL PASSWORD TUNGGU SEBE"
+clear
+echo ""$hi"INSTALL PASSWORD TUNGGU SEBEN"
+clear
+echo ""$hi"INSTALL PASSWORD TUNGGU SEBENT"
+clear
+echo ""$hi"INSTALL PASSWORD TUNGGU SEBENTA"
+clear
+echo ""$hi"INSTALL PASSWORD TUNGGU SEBENTAR"
+sleep 10
+clear
+echo ""$hi"[#.......................................................]   1%"
+sleep 0.5
+clear
+echo ""$hi"[##......................................................]   3%"
+sleep 0.5
+clear
+echo ""$hi"[###.....................................................]   8%"
+sleep 0.5
+clear
+echo ""$hi"[####....................................................]  10%"
+sleep 0.5
+clear
+echo ""$hi"[#####...................................................]  15%"
+sleep 0.5
+clear
+echo ""$hi"[######..................................................]  16%"
+sleep 0.5
+clear
+echo ""$hi"[#######.................................................]  18%"
+sleep 0.5
+clear
+echo ""$hi"[########................................................]  20%"
+sleep 0.5
+clear
+echo ""$hi"[#########...............................................]  25%"
+sleep 0.5
+clear
+echo ""$hi"[##########..............................................]  27%"
+sleep 0.5
+clear
+echo ""$hi"[##############..........................................]  30%"
+sleep 0.5
+clear
+echo ""$hi"[###############.........................................]  36%"
+sleep 0.5
+clear
+echo ""$hi"[################........................................]  38%"
+sleep 0.5
+clear
+echo ""$hi"[#################.......................................]  40%"
+sleep 0.5
+clear
+echo ""$hi"[##################......................................]  43%"
+sleep 0.5
+clear
+echo ""$hi"[###################.....................................]  46%"
+sleep 0.5
+clear
+echo ""$hi"[####################....................................]  48%"
+sleep 0.5
+clear
+echo ""$hi"[############################............................]  50%"
+sleep 0.5
+clear
+echo ""$hi"[#############################...........................]  52%"
+sleep 0.5
+clear
+echo ""$hi"[##############################..........................]  53%"
+sleep 0.5
+clear
+echo ""$hi"[###############################.........................]  56%"
+sleep 0.5
+clear
+echo ""$hi"[################################........................]  58%"
+sleep 0.5
+clear
+echo ""$hi"[###################################.....................]  62%"
+sleep 0.5
+clear
+echo ""$hi"[####################################....................]  66%"
+sleep 0.5
+clear
+echo ""$hi"[#####################################...................]  68%"
+sleep 0.5
+clear
+echo ""$hi"[######################################..................]  70%"
+sleep 0.5
+clear
+echo ""$hi"[#######################################.................]  72%"
+sleep 0.5
+clear
+echo ""$hi"[########################################................]  74%"
+sleep 0.5
+clear
+echo ""$hi"[#########################################...............]  76%"
+sleep 0.5
+clear
+echo ""$hi"[##########################################..............]  78%"
+sleep 0.5
+clear
+echo ""$hi"[###########################################.............]  80%"
+sleep 0.5
+clear
+echo ""$hi"[#################################################.......]  95%"
+sleep o.5
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[#.......................................................]   1%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[##......................................................]   3%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[###.....................................................]   8%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[####....................................................]  10%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[#####...................................................]  15%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[######..................................................]  16%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[#######.................................................]  18%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[########................................................]  20%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[#########...............................................]  25%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[##########..............................................]  27%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[##############..........................................]  30%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[###############.........................................]  36%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[################........................................]  38%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[#################.......................................]  40%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[##################......................................]  43%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[###################.....................................]  46%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[####################....................................]  48%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[############################............................]  50%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[#############################...........................]  52%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[##############################..........................]  53%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[###############################.........................]  56%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[################################........................]  58%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[###################################.....................]  62%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[####################################....................]  66%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[#####################################...................]  68%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[######################################..................]  70%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[#######################################.................]  72%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[########################################................]  74%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[#########################################...............]  76%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[##########################################..............]  78%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[###########################################.............]  80%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[#################################################.......]  95%"
+clear
+echo ""$hi"[########################################################] 100%"
+echo ""$hi"[########################################################] 100%"
+clear
+sleep 3
+echo ""
+figlet PWD CLAY
+echo ""
+echo ""$bl"╔════════════════════════════════════════════════╗"
+echo ""$bl"║"$ku"  *"$bl"    USER    "$pu" :"$bl" CLAY                          ║"
+echo ""$bl"║"$ku"  *"$bl"    PASSWORD"$pu" :"$bl" CLAY TEAM CYBER               ║"
+echo ""$bl"╚════════════════════════════════════════════════╝"
+until [ "$usr" = "CLAY" ]
+do
+read -p "user :" usr
+done
+until [ "$password" = "CLAY TEAM CYBER" ]
+do
+read -p "password :" password
+done
+clear
+figlet *CLAY TEAM*
+echo ""$bl"╔════════════════════════════════════════════════╗"
+echo ""$bl"║"$ku" *"$bl"  Author "$pu"  : "$bl"  AlvianCLAY01                   ║"
+echo ""$bl"║"$ku" *"$bl"  Contact"$pu"  : "$bl"  08**********                   ║"
+echo ""$bl"║"$ku" *"$bl"  G-mail "$pu"  : "$bl"  bdb*******@gmail.com           ║"
+echo ""$bl"║"$ku" *"$bl"  Team   "$pu"  : "$bl"  CLAY TEAM CYBER                ║"
+echo ""$bl"╚════════════════════════════════════════════════╝"
+echo ""
+echo ""
+echo ""$bi"[ "$pu"1"$bi" ]"$bi" "$bl"Trojans"$bi"                 ["$hi"♥"$bi"]"
+echo ""$bi"[ "$pu"2"$bi" ]"$bi" "$bl"SpamCall"$bi"                ["$hi"♥"$bi"]"
+echo ""$bi"[ "$pu"3"$bi" ]"$bi" "$bl"DarkFB"$bi"                  ["$hi"♥"$bi"]"
+echo ""$bi"[ "$pu"4"$bi" ]"$bi" "$bl"DDOS WIFI"$bi"               ["$hi"♥"$bi"]"
+echo ""$bi"[ "$pu"5"$bi" ]"$bi" "$bl"Perusak"$bi"                 ["$me"♥"$bi"]"
+echo ""$bl"[ "$pu"6"$bl" ]"$bl" "$bl"DDoS Website"$bi"            ["$hi"♥"$bl"]"
+echo ""$bl"[ "$pu"7"$bl" ]"$bi" "$bl"RED_HAWK"$bi"                ["$hi"♥"$bl"]"
+echo ""$bi"[ "$pu"0"$bi" ]"$bi" "$bl"Exit"$bi"                    ["$me"♥"$bi"]"
+echo ""$ku""
+echo "╔════════["$bl" CLAY TEAM CYBER"$ku" ]══["$bl" ♥ ♠ ♦ ♣"$ku" ]"
+read -p "╚═════[ pilih NO ]════# " pil
+if [ $pil = "1" ]:
+then
+echo "Installing Trojans..."
+cd $HOME
+apt update && apt upgrade
+apt install git -y
+apt install python -y
+apt install python2 -y
+git clone https://github.com/R133F/trojans
+echo ""echo ""$hi"Installing Succes"
+sleep 3
+cd trojans
+python2 trojans.py
+exit
+elif [ $pil = "2" ]:
+then
+clear
+echo "Installing SpamCall..."
+cd $HOME
+apt update && apt upgrade
+apt install git -y
+apt install figlet -y
+apt install python2 -y
+echo ""
+echo ""$hi"Installing Succes"
+sleep 3
+cd clay
+sh troll.sh
+exit
+elif [ $pil = "3" ]:
+then
+echo "installing DarkFB..."
+cd $HOME
+apt update && apt upgrade
+apt install python2 -y
+apt install git
+apt install php -y
+apt install ruby -y
+apt install toilet -y
+apt install figlet -ypip2 install -upgrade pip
+echo""
+echo ""$bi"Installing Succes"
+sleep 3
+cd clay
+python2 DarkFB.py
+exit
+elif [ $pil = "4" ]:
+then
+echo "installing DDOS WIFI..."
+cd $HOME
+apt update && apt upgrade
+apt install python2 -y
+apt install git -y
+apt install toilet -y
+echo ""
+echo ""$bi"Installing Succes"
+sleep 3
+cd clay
+python2 LITEDDOS.py
+exit
+elif [ $pil = "5" ]:
+then
+clear
+echo ""$ku"M" 
+clear 
+echo ""$ku"MA" 
+clear 
+echo ""$ku"MAA" 
+clear 
+echo ""$ku"MAAF" 
+clear 
+echo ""$ku"MAAF M" 
+clear 
+echo ""$ku"MAAF MA" 
+clear 
+echo ""$ku"MAAF MAS" 
+clear 
+echo ""$ku"MAAF MASI" 
+clear 
+echo ""$ku"MAAF MASIH" 
+clear 
+echo ""$ku"MAAF MASIH T" 
+clear 
+echo ""$ku"MAAF MASIH TA" 
+clear 
+echo ""$ku"MAAF MASIH TAH" 
+clear 
+echo ""$ku"MAAF MASIH TAHA" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP P" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP PE" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP PER" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP PERB" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP PERBA" 
+clear
+echo ""$ku"MAAF MASIH TAHAP PERBAI" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP PERBAIK" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP PERBAIKA" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP PERBAIKAN" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP PERBAIKAN *" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP PERBAIKAN *_" 
+clear 
+echo ""$ku"MAAF MASIH TAHAP PERBAIKAN *_*"
+sleep 1
+exit
+elif [ $pil = "6" ]:
+then
+echo ""$bl"Installing DDoS Website..."
+cd $HOME
+apt update && apt upgrade
+pkg install python -y2
+pkg install git -y
+pkg install toilet -y
+pkg install git -y
+git clone https://github.com/cyweb/hammer
+cd hammer
+echo ""$hi"Installing Succses"
+python hammer.py
+exit
+elif [ $pil = "7" ]:
+then
+echo ""$hi"Installing RED_HAWK..."
+cd $HOME
+apt update && apt upgrade
+pkg install php -y
+pkg install figlet -y
+pkg install git
+echo ""$hi"Installing Succses"
+cd clay
+php rhawk.php
+exit
+elif [ $pil = "0" ]:
+then
+echo ""$bl"╔════════════════════════════════════════════════╗"
+echo ""$bl"║   "$hi"             Terimakasih, ^_^     "$bl"           ║"
+echo ""$bl"╚════════════════════════════════════════════════╝"
+sleep 2
+exit
+else
+echo ""$bl"╔════════════════════════════════════════════════╗"
+echo ""$bl"║       "$me"            ERROR *_*     "$bl"               ║"
+echo ""$bl"╚════════════════════════════════════════════════╝"
+sleep 2
+echo $ulang
+fi
+done
+
